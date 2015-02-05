38caa5f0ce354f7ec115cd4a70da49c1a9282bd6
bonclay7 <koffirodrigue@gmail.com>
2015-02-05 16:55:47 +0100

    First test

--- tsa_questionnaire_api.py
+++ tsa_questionnaire_api.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+from flask.ext.restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+todos = {}
 
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id : todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id : todos[todo_id]}
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
+api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
