f104709c5d4a49a506c8599d68e98c3f172b6d81
bonclay7 <koffirodrigue@gmail.com>
2015-02-08 13:46:54 +0100

    Example from tutorial

--- tsa_questionnaire_api.py
+++ tsa_questionnaire_api.py
@@ -1,25 +1,53 @@
-from flask import Flask, request
-from flask.ext.restful import Resource, Api
+from flask import Flask
+from flask.ext.restful import Resource, Api, abort, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
 
-todos = {}
+TODOS = {
+    'todo1': {'task':'build an API'},
+    'todo2': {'task':'????'},
+    'todo3': {'task':'profit!'},
+}
 
-class TodoSimple(Resource):
+
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id  not in TODOS:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
+
+parser = reqparse.RequestParser()
+parser.add_argument('task', type=str)
+
+
+class Todo(Resource):
     def get(self, todo_id):
-        return {todo_id : todos[todo_id]}
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
+    def delete(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        return '', 204
 
     def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id : todos[todo_id]}
+        args = parser.parse_args()
+        task = {'task':args['task']}
+        TODOS[todo_id] = task
+        return task, 201
 
-api.add_resource(TodoSimple, '/<string:todo_id>')
-api.add_resource(HelloWorld, '/')
+class TodoList(Resource):
+    def get(self):
+        return TODOS
+
+    def post(self):
+        args = parser.parse_args()
+        todo_id = 'todo%d' % (len(TODOS) +1)
+        TODOS[todo_id] = {'task':args['task']}
+        return TODOS[todo_id], 201
+
+
+api.add_resource(TodoList, '/todos')
+api.add_resource(Todo, '/todos/<string:todo_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
